348cc80bab0c94d69c98daaf539da447f57558d5
Jordan Banaliev <jordanbanaliev@gmail.com>
2026-02-01 20:13:29 +0200

    feat: add EF Migrations for database portability and update project dependencies

---
+++ Migrations/20260201180927_InitialCreate.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SafetyWings.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "HealthLogs",
+                columns: table => new
+                {
+                    LogID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserID = table.Column<int>(type: "int", nullable: false),
+                    FlightID = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HeartRate = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OxygenLevel = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Temperature = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StressIndex = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HealthLogs", x => x.LogID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserRole = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HealthLogs");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
