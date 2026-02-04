50406509ecf3527b7bce54496bdceaaea37156c6
Jordan Banaliev <jordanbanaliev@gmail.com>
2026-02-04 13:52:40 +0200

    feat: added a working button for ext
    -added a bit of decoration to the other content

--- Client/js/dashboard.js
+++ Client/js/dashboard.js
@@ -1,7 +1,6 @@
-const exitBtn = document.getElementById('exit-btn');
-
-exitBtn.addEventListener('click', () => {
-    sessionStorage.clear();
-    console.log("Системите се изключват... Довиждане, пилот!");
-    // Тук по-късно ще добавим логика за изход от системата
-    console.log("Излизане от таблото...");});
+function logout(){
+    localStorage.removeItem('token');
+    localStorage.removeItem('username');
+    window.location.href = 'index.html';
+    console.log("Exit succesfully");
+}
